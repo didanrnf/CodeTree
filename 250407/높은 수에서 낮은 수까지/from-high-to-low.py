5125fbb052f074116c3e60c90022ca5435163090
didanrnf <45034147+didanrnf@users.noreply.github.com>
2025-04-07 23:20:28 +0900

    Title: 높은 수에서 낮은 수까지, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250407/높은 수에서 낮은 수까지/from-high-to-low.py
+++ 250407/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -3,6 +3,6 @@ a, b = map(int, input().split())
 if a > b:
     for i in range(a, b-1, -1):
         print(i, end = ' ')
-if b > a:
+else:
     for i in range(b, a-1, -1):
         print(i, end = ' ')
