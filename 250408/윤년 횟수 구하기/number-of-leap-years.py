a389c32dde527213cad705dbbee940bc47f85389
didanrnf <45034147+didanrnf@users.noreply.github.com>
2025-04-08 23:33:28 +0900

    Title: 윤년 횟수 구하기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250408/윤년 횟수 구하기/number-of-leap-years.py
@@ -0,0 +1,10 @@
+a = int(input())
+cnt = 0
+for i in range(1, a+1):
+    if i % 4 == 0:
+        cnt += 1
+        if i % 100 == 0 and i % 400 != 0:
+            cnt -= 1
+        
+        
+print(cnt)
