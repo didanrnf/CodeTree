9ff0858cb22f25a0619ebacaa775f1af625c6065
didanrnf <45034147+didanrnf@users.noreply.github.com>
2025-10-01 16:47:30 +0900

    Title: DateTime to DateTime, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251001/DateTime to DateTime/datetime-to-datetime.py
+++ 251001/DateTime to DateTime/datetime-to-datetime.py
@@ -8,12 +8,12 @@ minutes = c -11
 
 if minutes < 0:
     minutes += 60
-    minutes -= 1
+    hour -= 1
 
 if hour < 0:
     hour += 24
     day -= 1
-    
+  
 sum_minutes = day*24*60 + hour*60 + minutes
 
 if a < 11 and b < 11 and c < 11:
